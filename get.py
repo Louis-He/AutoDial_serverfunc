c601a594be092b91ff1b2a75e65c6ad5154e9b0f
Louis-He <1726110778@qq.com>
2017-11-10 16:39:56 -0500

    updated

--- get.py
+++ get.py
@@ -7,20 +7,18 @@ urls = (
 
 class index:
     def GET(self, name):
-        try:
-            print web.input()
-            i = web.input()
-            ID = i.ID
-            lon = float(i.lon)
-            lat = float(i.lat)
-            print ID, lon, lat
+        print web.input()
+        i = web.input()
+        ID = i.ID
+        lon = float(i.lon)
+        lat = float(i.lat)
+        print ID, lon, lat
 
-            f = open('/root/web/posrecord.txt', 'a+')
-            f.write('[' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ']\t' + ID + '\t' + lon + '\t' + lat + '\tPOSITION SAVED\n')
-            f.close()
-            return "SUCCEED"
-        except:
-            return "FAIL"
+        f = open('/root/web/posrecord.txt', 'a+')
+        f.write('[' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ']\t' + ID + '\t' + lon + '\t' + lat + '\tPOSITION SAVED\n')
+        f.close()
+        return "SUCCEED"
+
 
 if __name__ == "__main__":
     f = open('/root/web/posrecord.txt', 'w+')
